198795f5a37ac71ed9715f6d1afec06630ee7726
dczysz <32844299+dczysz@users.noreply.github.com>
2020-01-21 17:49:26 -0700

    Added some meta tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,22 @@
+import { useRouter } from 'next/router';
+import Head from 'next/head';
+
 import Header from './Header';
 
 const Layout: React.FC = ({ children }) => {
+  const router = useRouter();
+
   return (
     <>
+      <Head>
+        <title>Water Waste {router.query.name || ''}</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="theme-color" content="#4589dd" />
+        <meta
+          name="description"
+          content="How much water does food production use?"
+        />
+      </Head>
       <Header />
       <main>{children}</main>
     </>
