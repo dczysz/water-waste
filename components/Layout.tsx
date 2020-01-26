c30b0c51f17cf6b8e824f8a74e6792dcabb73349
dczysz <32844299+dczysz@users.noreply.github.com>
2020-01-26 16:05:56 -0700

    Added favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,12 +12,33 @@ const Layout: React.FC = ({ children }) => {
         <title>
           Water Waste{router.query.name ? ` - ${router.query.name}` : ''}
         </title>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="theme-color" content="#4589dd" />
         <meta
           name="description"
           content="How much water does food production use?"
         />
+
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4d4c6b" />
+        <meta name="msapplication-TileColor" content="#4d4c6b" />
+        <meta name="theme-color" content="#4d4c6b" />
       </Head>
       <Header />
       <main>{children}</main>
